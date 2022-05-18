3122ffe3ec6ea868b0008b6bef7907c82593ad5d
Your Name <vickym03>
2022-05-18 14:32:37 +0530

    client

---
+++ DataMang_Client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';import './App.css';
+import Upload from './component/Upload';
+
+function App() {
+  return (
+    <div className="App">
+     <Upload/>
+    </div>
+  );
+}
+
+export default App;
